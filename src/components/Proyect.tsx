d62323295e8eef0465f9b5df5fa6dec91d946c48
cesarManjaro <cesarwamartinez@gmail.com>
2025-02-13 07:45:58 -0500

    Changes link proyect

--- src/components/Proyect.tsx
+++ src/components/Proyect.tsx
@@ -3,23 +3,18 @@ import TiltedCard from "./Card";
 import { ItemProyect } from "../lib/response";
 import { Ref } from "react";
 
-
 interface ProyectProps {
-    divProject: Ref<HTMLDivElement>
-    data: ItemProyect[]
+  divProject: Ref<HTMLDivElement>;
+  data: ItemProyect[];
 }
 
-
-export default function Proyect({divProject, data}:ProyectProps) {
+export default function Proyect({ divProject, data }: ProyectProps) {
   return (
-    <div
-      className="p-12 w-full flex gap-6 flex-col "
-      ref={divProject}
-    >
+    <div className="p-12 w-full flex gap-6 flex-col" ref={divProject}>
       <div className="w-full grid place-content-center">
         <SplitText
           text="Mis proyectos"
-          className="text-lg text-zinc-300 font-medium"
+          className="text-2xl text-zinc-300 font-medium"
           textAlign="left"
           delay={20}
           animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
@@ -30,21 +25,21 @@ export default function Proyect({divProject, data}:ProyectProps) {
       </div>
       <div className="grid grid-cols-5 gap-6">
         {data.map((item) => (
-          <TiltedCard
-            imageSrc={`/src/assets/${item.image}`}
-            altText={`${item.name}`}
-            captionText={`${item.description}`}
-            rotateAmplitude={12}
-            scaleOnHover={1.1}
-            showMobileWarning={false}
-            showTooltip={true}
-            displayOverlayContent={true}
-            overlayContent={
-              <p className="tilted-card-demo-text shiny-button backdrop-blur-2xl p-0.5 rounded-lg  ">
-                {item.name}
-              </p>
-            }
-          />
+            <TiltedCard
+              imageSrc={`/src/assets/${item.image}`}
+              altText={`${item.name}`}
+              captionText={`${item.description}`}
+              rotateAmplitude={12}
+              scaleOnHover={1.1}
+              showMobileWarning={false}
+              showTooltip={true}
+              displayOverlayContent={true}
+              overlayContent={
+                <a className="tilted-card-demo-text shiny-button backdrop-blur-2xl p-0.5 rounded-lg" href={item.link} >
+                  {item.name}
+                </a>
+              }
+            />
         ))}
       </div>
     </div>

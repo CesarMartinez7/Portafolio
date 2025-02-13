dc989c9a8dadee0ecb0ab22bfbebf7a4b9525cd3
cesarManjaro <cesarwamartinez@gmail.com>
2025-02-13 08:21:38 -0500

    Fix image deploy

--- src/components/Proyect.tsx
+++ src/components/Proyect.tsx
@@ -26,7 +26,7 @@ export default function Proyect({ divProject, data }: ProyectProps) {
       <div className="grid grid-cols-5 gap-6">
         {data.map((item) => (
             <TiltedCard
-              imageSrc={`/src/assets/${item.image}`}
+              imageSrc={`/public/${item.image}`}
               altText={`${item.name}`}
               captionText={`${item.description}`}
               rotateAmplitude={12}

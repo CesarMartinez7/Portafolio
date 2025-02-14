6a65c1a323275b5fde2f25c9ad83fcabea60fdf5
cesarManjaro <cesarwamartinez@gmail.com>
2025-02-14 14:45:49 -0500

    Changes fonts

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -17,8 +17,8 @@ function Main() {
 
   return (
     <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1}}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.6}}
     >

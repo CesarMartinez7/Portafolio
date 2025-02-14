afcc5acd44e6b8c0a542cdc5788067bf8d41a093
cesarManjaro <cesarwamartinez@gmail.com>
2025-02-14 02:06:53 -0500

    Mejor letter habilidades

--- src/components/Habilidades.tsx
+++ src/components/Habilidades.tsx
@@ -18,13 +18,13 @@ export default function Habilidades() {
     <Particles></Particles>
     <div className="h-screen w-full grid place-content-center">
       <div className="flex justify-center">
-        <h2 className=" text-[44px] font-medium bg-gradient-to-t from-zinc-600 to-zinc-200 bg-clip-text text-transparent ">
+        <h2 className=" text-[44px] font-medium bg-gradient-to-t from-zinc-600 to-zinc-200 bg-clip-text text-transparent text-center ">
           Lenguajes y herramientas que utilizo
         </h2>
       </div>
       <div className="relative z-10">
         <div className="absolute w-full h-full bg-gradient-to-b z-50 from-[#101010] to-transparent"></div>
-        <div className="grid grid-cols-3 p-2.5 lg:grid-cols-8 lg:px-44 lg:py-12 gap-5 z-10 ">
+        <div className="grid grid-cols-4 md:grid-cols-6 p-8 lg:grid-cols-8 lg:px-44 lg:py-12 gap-5 z-10  ">
           {ToolJson.map((tool, index) => (
             <SpotlightCard
               key={index}

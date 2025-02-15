8aabd6e702ac7fedd6c30ad4e106e6c1e1ee46d2
cesarManjaro <cesarwamartinez@gmail.com>
2025-02-15 00:09:47 -0500

    Change proyect

--- src/page/ProyectPage.tsx
+++ src/page/ProyectPage.tsx
@@ -38,7 +38,7 @@ export default function ProyectPage() {
             text={`${data.name}`}
             className="text-5xl"
             textAlign="left"
-            delay={80}
+            delay={40}
             animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
             animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
             threshold={0.2}

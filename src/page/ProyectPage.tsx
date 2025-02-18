860118c9c6334798081815043fc33fa023c2d168
cesarManjaro <cesarwamartinez@gmail.com>
2025-02-18 08:04:37 -0500

    Add all nice tecnologias

--- src/page/ProyectPage.tsx
+++ src/page/ProyectPage.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import SpotlightCard from "../components/SpothCard";
-import BlurText from "../components/Blurtext";
 
 import { Safari } from "../components/Mockup";
 
